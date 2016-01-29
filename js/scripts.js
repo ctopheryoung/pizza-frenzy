455eb4b02c81007fe451b2cd940057fc44a3fab3
Chris Young <ctopheryoung@gmail.com>
2016-01-29 09:55:01 -0800

    pass 2nd spec to generate price based on Pizza object values

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,28 @@ function Pizza(pieSize, meatToppings, veggieToppings) {
   this.pieSize = pieSize;
   this.meatToppings = meatToppings;
   this.veggieToppings = veggieToppings;
+  this.price = 0;
+};
+
+Pizza.prototype.generatePrice = function () {
+  var price = 0;
+  for (var i = 0; i < this.meatToppings; i++) {
+    price += 2
+  }
+  for (var i = 0; i < this.veggieToppings; i++) {
+    price += 1;
+  }
+  if (this.pieSize === "small") {
+    price += 14;
+  }
+  if (this.pieSize === "medium") {
+    price += 16;
+  }
+  if (this.pieSize === "large") {
+    price += 18;
+  }
+  if (this.pieSize === "x-large") {
+    price += 20;
+  };
+  return price;
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,15 @@
-describe ("Pizza", function (){
+describe ("Pizza", function () {
   it("will create a Pizza object with values for size, meat toppings, and veggie toppings", function() {
   var testPizza = new Pizza ("large", 1, 2);
   expect(testPizza.pieSize).to.equal("large");
   expect(testPizza.meatToppings).to.equal(1);
   expect(testPizza.veggieToppings).to.equal(2);
+  });
+});
+
+describe ("generatePrice", function () {
+  it("will calculate a price for a pizza based on size, number of meat toppings, and number of veggie toppings", function () {
+    var testPizza = new Pizza ("large", 1, 2);
+    expect(testPizza.generatePrice()).to.equal(22);
   });
 });
